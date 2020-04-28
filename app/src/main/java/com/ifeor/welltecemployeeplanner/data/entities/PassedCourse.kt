fdfde4edea15e63f68ad2a5e6bfd7b02941cf2f7
IFEOR <nazumy@bk.ru>
2020-04-28 13:34:38 +0600

    [+] Room Entities started

--- app/src/main/java/com/ifeor/welltecemployeeplanner/data/entities/PassedCourse.kt
+++ app/src/main/java/com/ifeor/welltecemployeeplanner/data/entities/PassedCourse.kt
@@ -1,4 +1,15 @@
 package com.ifeor.welltecemployeeplanner.data.entities
 
+import androidx.room.Entity
+import java.text.DateFormat
+
+@Entity
 class PassedCourse {
+
+    // @ForeignKey
+    var courseID: Long = 0
+    // @ForeignKey
+    var employeeID: Long = 0
+
+    //var passedDate: DateFormat() = DateFormat()
 }
--- app/src/main/java/com/ifeor/welltecemployeeplanner/data/entities/Site.kt
+++ app/src/main/java/com/ifeor/welltecemployeeplanner/data/entities/Site.kt
@@ -1,4 +1,17 @@
 package com.ifeor.welltecemployeeplanner.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 class Site {
+
+    @PrimaryKey
+    var siteID: Long = 0
+    //@ForeignKey(entity = notificationID, parentColumns = Notification, childColumns = notificationID)
+    var locationID: Long = 0
+    //@ForeignKey(entity = employeeID, parentColumns = Employee, childColumns = employeeID)
+    var employeeID: Long = 0
+
+    //var siteStartDate: DateFormat = DateFormat()
 }

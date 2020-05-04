a363361bf39143422b13ab621578faffdd5a9988
IFEOR <nazumy@bk.ru>
2020-05-04 14:38:38 +0600

    [*] Getting internet enable started

--- app/src/main/java/com/ifeor/welltecemployeeplanner/ui/presenters/NotificationListPresenter.kt
+++ app/src/main/java/com/ifeor/welltecemployeeplanner/ui/presenters/NotificationListPresenter.kt
@@ -1,5 +1,10 @@
 package com.ifeor.welltecemployeeplanner.ui.presenters
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.util.Log
+import com.ifeor.welltecemployeeplanner.data.FirestoneDatabase
 import com.ifeor.welltecemployeeplanner.data.repositories.NotificationRepositoryImpl
 import com.ifeor.welltecemployeeplanner.ui.views.NotificationListView
 import kotlinx.coroutines.Dispatchers
@@ -8,6 +13,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import moxy.InjectViewState
 import moxy.MvpPresenter
+
 
 @InjectViewState
 class NotificationListPresenter: MvpPresenter<NotificationListView>() {
@@ -20,10 +26,19 @@ class NotificationListPresenter: MvpPresenter<NotificationListView>() {
             try {
                 val notifications = notificationRepository.fetchNotificationAsync().await()
                 withContext(Dispatchers.Main) {
-                    viewState.presentNotifications(data = notifications)
+                    if (!notifications.isEmpty()) {
+                        viewState.presentNotifications(data = notifications)
+                    } else {
+                        if(false) // TODO
+
+                            viewState.showLoadErrorText()
+                        else
+                            viewState.showNoDataText()
+                    }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
+                viewState.showLoadErrorText()
             }
         }
     }

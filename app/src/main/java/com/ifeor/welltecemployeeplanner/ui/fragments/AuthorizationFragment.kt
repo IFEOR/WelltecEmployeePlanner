1de4d3b9e8103d925e1410113a7b8db156c231f6
IFEOR <nazumy@bk.ru>
2020-05-04 21:39:52 +0600

    [+] User authentication

--- app/src/main/java/com/ifeor/welltecemployeeplanner/ui/fragments/AuthorizationFragment.kt
+++ app/src/main/java/com/ifeor/welltecemployeeplanner/ui/fragments/AuthorizationFragment.kt
@@ -1,16 +1,17 @@
 package com.ifeor.welltecemployeeplanner.ui.fragments
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import com.ifeor.welltecemployeeplanner.R
 import com.ifeor.welltecemployeeplanner.ui.activities.LoginActivity
 import kotlinx.android.synthetic.main.fragment_authorization.*
 
-class AuthorizationFragment: Fragment() {
+class AuthorizationFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +24,7 @@ class AuthorizationFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fragment_auth_button.setOnClickListener {onSignInListener() }
+        fragment_auth_button.setOnClickListener { onSignInListener() }
         fragment_auth_sign_up.setOnClickListener { onSignUp() }
     }
 
@@ -32,20 +33,37 @@ class AuthorizationFragment: Fragment() {
     }
 
     private fun onSignInListener() {
-        if(isValidFields()) {
-            // TODO Firebase and password check
-            // TODO Navigate to MainActivity
+        if (isValidFields()) {
+            (activity as LoginActivity).auth.signInWithEmailAndPassword(
+                fragment_auth_username.text.toString(),
+                fragment_auth_password.text.toString()
+            )
+                .addOnCompleteListener(activity as LoginActivity) { task ->
+                    if (task.isSuccessful) {
+                        val user = (activity as LoginActivity).auth.currentUser
+                        (activity as LoginActivity).updateUI(user)
+                    } else {
+                        Toast.makeText(
+                            activity as LoginActivity, "Authentication failed. Try again after some time",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 
     private fun isValidFields(): Boolean {
         var isNotError = true
         if (fragment_auth_username.text.isEmpty()) {
-            fragment_auth_username.error = R.string.error_empty.toString()
+            fragment_auth_username.error = getString(R.string.error_empty)
+            isNotError = false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(fragment_auth_username.text.toString()).matches()) {
+            fragment_auth_username.error = getString(R.string.error_email)
             isNotError = false
         }
         if (fragment_auth_password.text.isEmpty()) {
-            fragment_auth_password.error = R.string.error_empty.toString()
+            fragment_auth_password.error = getString(R.string.error_empty)
             isNotError = false
         }
         return isNotError
--- app/src/main/java/com/ifeor/welltecemployeeplanner/ui/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/ifeor/welltecemployeeplanner/ui/fragments/RegistrationFragment.kt
@@ -1,14 +1,17 @@
 package com.ifeor.welltecemployeeplanner.ui.fragments
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ifeor.welltecemployeeplanner.R
+import com.ifeor.welltecemployeeplanner.ui.activities.LoginActivity
 import kotlinx.android.synthetic.main.fragment_registration.*
 
-class RegistrationFragment: Fragment() {
+class RegistrationFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,43 +28,64 @@ class RegistrationFragment: Fragment() {
     }
 
     private fun onSignUpListener() {
-        if(isValidFields()) {
-
+        if (isValidFields()) {
+            (activity as LoginActivity).auth.createUserWithEmailAndPassword(
+                fragment_registration_email.text.toString(),
+                fragment_registration_password.text.toString()
+            )
+                .addOnCompleteListener(activity as LoginActivity) { task ->
+                    if (task.isSuccessful) {
+                        (activity as LoginActivity).toAuth()
+                        Toast.makeText(
+                            activity as LoginActivity, "You have successfully registered",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            activity as LoginActivity, "Sign Up failed. Try again after some time",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 
     private fun isValidFields(): Boolean {
         var isNotError = true
         if (fragment_registration_key.text.isEmpty()) {
-            fragment_registration_key.error = R.string.error_empty.toString()
+            fragment_registration_key.error = getString(R.string.error_empty)
             isNotError = false
         }
         if (fragment_registration_first.text.isEmpty()) {
-            fragment_registration_first.error = R.string.error_empty.toString()
+            fragment_registration_first.error = getString(R.string.error_empty)
             isNotError = false
         }
         if (fragment_registration_second.text.isEmpty()) {
-            fragment_registration_second.error = R.string.error_empty.toString()
+            fragment_registration_second.error = getString(R.string.error_empty)
             isNotError = false
         }
         if (fragment_registration_email.text.isEmpty()) {
-            fragment_registration_email.error = R.string.error_empty.toString()
+            fragment_registration_email.error = getString(R.string.error_empty)
+            isNotError = false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(fragment_registration_email.text.toString()).matches()) {
+            fragment_registration_email.error = getString(R.string.error_email)
             isNotError = false
         }
         if (fragment_registration_phone.text.isEmpty()) {
-            fragment_registration_phone.error = R.string.error_empty.toString()
+            fragment_registration_phone.error = getString(R.string.error_empty)
             isNotError = false
         }
         if (fragment_registration_position.text.isEmpty()) {
-            fragment_registration_position.error = R.string.error_empty.toString()
+            fragment_registration_position.error = getString(R.string.error_empty)
             isNotError = false
         }
         if (fragment_registration_password.text.length < 8) {
-            fragment_registration_password.error = R.string.error_short.toString()
+            fragment_registration_password.error = getString(R.string.error_short)
             isNotError = false
         }
-        if (fragment_registration_re_password.text != fragment_registration_password.text) {
-            fragment_registration_re_password.error = R.string.error_differ_password.toString()
+        if (fragment_registration_re_password.text.toString() != fragment_registration_password.text.toString()) {
+            fragment_registration_re_password.error = getString(R.string.error_differ_password)
             isNotError = false
         }
         return isNotError
